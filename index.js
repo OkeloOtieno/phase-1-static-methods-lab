e451f0f4d19aa46ceb053607d7b2164a746f0187
Emmanuel <emmanuelokello294@gmail.com>
2024-04-11 10:33:47 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+      return str.replace(/[^a-zA-Z0-9\s'-]/g, '');
+  }
+
+  static titleize(str) {
+      const wordsToIgnore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+      return str.split(' ')
+              .map((word, index) => {
+                    if (index === 0 || !wordsToIgnore.includes(word.toLowerCase())) {
+                        return Formatter.capitalize(word);
+                    }
+                    return word;
+              })
+              .join(' ');
+  }
 }
